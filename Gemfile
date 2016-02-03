68e651181f2eaf337dcf3f2597f3e7de656c738c
Juan González <juan.gonzalez@xing.com>
2016-02-03 18:49:17 +0100

    Base gem as created by bundle

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in xing-pdf_cover.gemspec
+gemspec
---
+++ lib/xing/pdf_cover.rb
@@ -0,0 +1,7 @@
+require "xing/pdf_cover/version"
+
+module Xing
+  module PdfCover
+    # Your code goes here...
+  end
+end
