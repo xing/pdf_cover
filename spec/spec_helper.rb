66176219245eb62a1f72c2221c63f70f9347d81d
Juan González <juan.gonzalez@xing.com>
2016-02-08 16:20:01 +0100

    Include paperclip matchers and disable warnings on specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,19 +7,21 @@ SimpleCov.start
 # Development dependencies
 require "byebug"
 require "paperclip"
+require "paperclip/matchers"
 require "carrierwave"
 
 # The gem itself
 require "xing/pdf_cover"
 
 RSpec.configure do |config|
+  config.include Paperclip::Shoulda::Matchers
+
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
 
   config.disable_monkey_patching!
 
-  config.warnings = true
   config.profile_examples = 10
   config.order = :random
   config.default_formatter = "Fivemat"
