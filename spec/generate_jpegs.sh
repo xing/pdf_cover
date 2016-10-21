665bbf7d07e9284e2759d036394eb2b70aae4041
Alejandro Dev <aeinformatico@gmail.com>
2016-10-21 15:10:14 +0200

    Add default antialias
    Values should be between 1 and 4.
    For more information refer to:
    
    http://ghostscript.com/doc/current/Devices.htm#Image+file+formats

--- spec/generate_jpegs.sh
+++ spec/generate_jpegs.sh
@@ -4,6 +4,7 @@
 DEFAULT_FORMAT="jpeg"
 DEFAULT_QUALITY=85
 DEFAULT_RESOLUTION=300
+DEFAULT_ANTIALIASING=4
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/test_pdfs"
 
@@ -19,5 +20,14 @@ for INPUT_FILE in $(cd $DIR && ls *.pdf)
 do
   OUTPUT_FILE="$DIR/${INPUT_FILE/.pdf/.jpg}"
   echo "Generating $OUTPUT_FILE from $INPUT_FILE"
-  gs -sOutputFile="$OUTPUT_FILE" -dNOPAUSE -sDEVICE="$DEFAULT_FORMAT" -dJPEGQ=$DEFAULT_QUALITY -dFirstPage=1 -dLastPage=1 -r$DEFAULT_RESOLUTION -q "$DIR/$INPUT_FILE" -c quit
+  gs -sOutputFile="$OUTPUT_FILE" \
+    -dNOPAUSE \
+    -sDEVICE="$DEFAULT_FORMAT" \
+    -dJPEGQ=$DEFAULT_QUALITY \
+    -dFirstPage=1 \
+    -dLastPage=1 \
+    -r$DEFAULT_RESOLUTION \
+    -dTextAlphaBits=$DEFAULT_ANTIALIASING \
+    -dGraphicsAlphaBits=$DEFAULT_ANTIALIASING \
+    -q "$DIR/$INPUT_FILE" -c quit
 done
